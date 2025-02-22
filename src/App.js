b93916b26c8829ed84b957287a276c567f686686
Manojkumar143341 <manoj166174@gmail.com>
2025-02-22 22:23:05 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+    Routes, Route,
+    Link, useLocation
+} from 'react-router-dom';
+import Appointments from './components/Appointments';
+import Doctors from './components/Doctors';
+import Patients from './components/Patients';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="container">
+            <Nav />
+            <Routes>
+                <Route path="/appointments" element={<Appointments />} />
+                <Route path="/" element={<Appointments />} />
+                <Route path="/doctors" element={<Doctors />} />
+                <Route path="/patients" element={<Patients />} />
+            </Routes>
+        </div>
+    );
+}
+
+const Nav = () => {
+    const location = useLocation(); // Use location inside the Nav component
+
+    const isLinkActive = (path) => location.pathname === path;
+
+    return (
+        <div className="container">
+            <h1 style={{ color: "green" }}>
+                GFG - Hospital Management App
+            </h1>
+            <nav>
+                <ul>
+                    <li className={isLinkActive('/appointments') ? 'active' : ''}>
+                        <Link to="/appointments">Appointments</Link>
+                    </li>
+                    <li className={isLinkActive('/doctors') ? 'active' : ''}>
+                        <Link to="/doctors">Doctors</Link>
+                    </li>
+                    <li className={isLinkActive('/patients') ? 'active' : ''}>
+                        <Link to="/patients">Patients</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
 export default App;
